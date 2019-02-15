0be3ace6035950ef12c40af2aeee94d4689e9cf3
Aaron You <aarony@synnex.com>
2019-02-15 14:47:38 +0800

    add trains test case

---
+++ src/main/java/com/ysl/tw/graph/RouteNetwork.java
@@ -0,0 +1,30 @@
+package com.ysl.tw.graph;
+
+/**
+ * @author aarony
+ * @Created 2019-02-14 13:31
+ * @E-mail aarony@synnex.com
+ */
+public class RouteNetwork {
+
+    public Integer caculateDistance(Station... stations){
+        return 0;
+    }
+
+    public Integer howTripWithMaxStop(Station start,Station end,Integer maxStop){
+        return 0;
+    }
+
+    public Integer howTripWithExactStop(Station start,Station end,Integer exactStop){
+        return 0;
+    }
+
+    public Integer getShortestLenWithStops(Station start,Station end){
+        return 0;
+    }
+
+    public Integer getTripQtyWithLimitDis(Station start,Station end,Integer maxDis){
+        return 0;
+    }
+
+}
---
+++ src/main/java/com/ysl/tw/graph/Station.java
@@ -0,0 +1,14 @@
+package com.ysl.tw.graph;
+
+/**
+ * @author aarony
+ * @Created 2019-02-14 13:49
+ * @E-mail aarony@synnex.com
+ */
+public class Station {
+    private String name;
+
+    public Station(String name) {
+        this.name = name;
+    }
+}
